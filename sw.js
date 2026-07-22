56aaf7c026557b8a2d583dd7a22266e1d4f83750
ndlong78 <ndlong78@gmail.com>
2026-07-22 17:34:08 +0700

    chore(pwa): cache standardized theme content path

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 /* BÔNG HOME'S - Service Worker */
-const PHIEN_BAN = "bonghome-v17-themes-strict-fix";
+const PHIEN_BAN = "bonghome-v18-theme-content-paths";
 const DANH_SACH_LUU = [
   "./", "./index.html",
   "./game1.html", "./game2.html", "./game3.html", "./game4.html", "./game5.html", "./game6.html", "./game7.html", "./game8.html", "./game9.html", "./game10.html",
   "./shared-ui.js", "./pwa-ios.js", "./pwa-quality.js", "./game1-difficulty.js", "./game1-autosave.js",
   "./js/storage.js", "./js/themes.js", "./js/theme-picker.js", "./js/progress.js", "./js/game1-theme-progress.js", "./js/game1-content.js",
-  "./content/games/game1.json", "./content/games/game1-animals.json", "./content/themes/index.json",
+  "./content/games/game1.json", "./content/themes/animals/game1.json", "./content/themes/index.json",
   "./css/design-tokens.css", "./css/themes.css", "./css/theme-picker.css", "./css/common.css", "./css/components.css", "./css/game1-autosave.css",
   "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./apple-touch-icon.png"
 ];
