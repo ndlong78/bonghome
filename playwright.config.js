9c57a1e73e005b073149dfed3f4056e0d2ef689e
ndlong78 <ndlong78@gmail.com>
2026-07-27 18:54:13 +0700

    Use stable Chromium headless mode in CI

--- playwright.config.js
+++ playwright.config.js
@@ -12,6 +12,7 @@ module.exports = defineConfig({
   use: {
     baseURL: 'http://127.0.0.1:4173',
     browserName: 'chromium',
+    channel: process.env.CI ? 'chromium' : undefined,
     locale: 'vi-VN',
     serviceWorkers: 'allow',
     trace: 'retain-on-failure',
