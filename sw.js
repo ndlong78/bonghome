30aa75bebdd8c9d66b246e92b1cc0dad5c88b860
ndlong78 <ndlong78@gmail.com>
2026-07-28 14:41:38 +0700

    Bump cache for Game 2-4 autosave routes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BÔNG HOME'S - Service Worker */
-const PHIEN_BAN = "bonghome-v63-game1-autosave-route-api";
+const PHIEN_BAN = "bonghome-v64-games2-4-autosave-routes";
 const TEN_CACHE = Object.freeze({
   shell: `${PHIEN_BAN}-shell`,
   games: `${PHIEN_BAN}-games`,
