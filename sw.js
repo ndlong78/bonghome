f5bd4864314fab0ea1a469a6a9b808f778c6ef8b
ndlong78 <ndlong78@gmail.com>
2026-09-12 18:46:19 +0700

    Bump PWA cache for Game 11 pointer follow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BÔNG HOME'S - Service Worker */
-const PHIEN_BAN = "bonghome-v68-game11-click-rotate";
+const PHIEN_BAN = "bonghome-v69-game11-pointer-follow";
 const TEN_CACHE = Object.freeze({
   shell: `${PHIEN_BAN}-shell`,
   games: `${PHIEN_BAN}-games`,
